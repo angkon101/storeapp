d4637ef94dba78ac96a38856f90cc04d83dbfc09
Angkon Debnath <angkonnath1@gmail.com>
2024-12-31 00:21:43 +0600

    This is the initial code, I will change it later.

---
+++ pink.cpp
@@ -0,0 +1,48 @@
+                //         Angkon Debnath
+                //           CSE,IUBAT
+                //    Miles to go before I sleep
+
+
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+
+
+#define fst_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+
+
+#define ll                         long long
+#define ull                        unsigned long long
+#define nl                         endl
+#define N                          1e6
+
+
+#define mini3(a,b,c)                min(a,min(b,c))
+#define mini4(a,b,c,d)              min(a,min(b,min(c,d)))
+#define maxi3(a,b,c)                max(a,max(b,c))
+#define maxi4(a,b,c,d)              max(a,max(b,max(c,d)))
+
+
+#define arrsort                     sort(arr, arr+n)
+#define arrinput                    for(int i=0; i<n; i++){ cin>>arr[i];}
+#define arroutput                   for(int i=0; i<n; i++){cout<<arr[i];}
+
+
+
+void angkon_taste_the_code()
+{   
+
+    cout<<"Hello world";
+ 
+}
+
+int main()
+{
+    fst_io
+    // int t; cin>>t;
+    // while(t--) 
+    angkon_taste_the_code();
+}
